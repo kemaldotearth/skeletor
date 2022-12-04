4022bec1d10fc74cf6ae95c85e3a3f4ed67a4d7c
iamanoctopus <yes@iamerd.xyz>
2022-12-04 21:40:40 +0100

    feat: first command

--- packages/skeletor-cli/src/index.ts
+++ packages/skeletor-cli/src/index.ts
@@ -6,4 +6,25 @@ const program = new Command();
 program.description(
   'A simple CLI tool for generating front end design system packages.',
 );
-program.option('--verbose', 'verbose logging');
+program.option('--verbose', 'Verbose logging.');
+program.version('0.1.0', '-v, --version', 'Logs current version.');
+
+program.command('generate:tokens').action(async () => {
+  console.log('Generating tokens...');
+});
+
+const run = async () => {
+  await program.parseAsync();
+};
+
+run();
+
+process.on('unhandledRejection', function (err: Error) {
+  const debug = program.opts().verbose;
+
+  if (debug) {
+    console.error(err.stack);
+  }
+
+  program.error('', { exitCode: 1 });
+});
